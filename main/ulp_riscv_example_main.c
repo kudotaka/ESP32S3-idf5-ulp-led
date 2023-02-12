3b12ea0a3a325f2157173b016bbd996a07f4ade5
kudotaka <kudotaka@aurora.ocn.ne.jp>
2023-02-12 19:13:12 +0900

    Add Button

--- main/ulp_riscv_example_main.c
+++ main/ulp_riscv_example_main.c
@@ -1,9 +1,8 @@
-/* ULP riscv DS18B20 1wire temperature sensor example
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
+// ULP riscv DS18B20 1wire temperature sensor example
+// This example code is in the Public Domain (or CC0 licensed, at your option.)
+// Unless required by applicable law or agreed to in writing, this
+// software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.
 
 #include <stdio.h>
 #include "esp_sleep.h"
@@ -16,42 +15,126 @@
 #include "ulp_main.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
+
+static const char *TAG = "MY-MAIN";
 
 extern const uint8_t ulp_main_bin_start[] asm("_binary_ulp_main_bin_start");
 extern const uint8_t ulp_main_bin_end[]   asm("_binary_ulp_main_bin_end");
 
-static void init_ulp_program(void);
+static void start_ulp_program(void);
+static void stop_ulp_program(void);
+static void restart_ulp_program(void);
 
 void app_main(void)
 {
+    vTaskDelay(pdMS_TO_TICKS(10));
+
     esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
-    /* not a wakeup from ULP, load the firmware */
+
+    // not a wakeup from ULP, load the firmware
     if (cause != ESP_SLEEP_WAKEUP_ULP) {
         printf("Not a ULP-RISC-V wakeup, initializing it! \n");
-        init_ulp_program();
+        start_ulp_program();
     }
 
-    /* ULP Risc-V read and detected a change in GPIO_0, prints */
+    // ULP Risc-V read and detected a change in GPIO_0, prints
     if (cause == ESP_SLEEP_WAKEUP_ULP) {
         printf("ULP-RISC-V woke up the main CPU! \n");
+        printf("ULP-RISC-V ulp running_cnt is %ld \n", ulp_running_cnt);
+        stop_ulp_program();
+        vTaskDelay(pdMS_TO_TICKS(10000));
     }
 
-    /* Go back to sleep, only the ULP Risc-V will run */
-    printf("Entering in deep sleep\n\n");
+    switch (cause)
+    {
+    case ESP_SLEEP_WAKEUP_UNDEFINED:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_UNDEFINED");
+        break;
 
-    /* Small delay to ensure the messages are printed */
-    vTaskDelay(100);
+    case ESP_SLEEP_WAKEUP_ALL:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_ALL");
+        break;
 
-    ESP_ERROR_CHECK( esp_sleep_enable_ulp_wakeup());
+    case ESP_SLEEP_WAKEUP_EXT0:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_EXT0");
+        break;
+    
+    case ESP_SLEEP_WAKEUP_EXT1:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_EXT1");
+        break;
+
+    case ESP_SLEEP_WAKEUP_TIMER:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_TIMER");
+        break;
+
+    case ESP_SLEEP_WAKEUP_TOUCHPAD:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_TOUCHPAD");
+        break;
+
+    case ESP_SLEEP_WAKEUP_ULP:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_ULP");
+        break;
+
+    case ESP_SLEEP_WAKEUP_GPIO:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_GPIO");
+        break;
+
+    case ESP_SLEEP_WAKEUP_UART:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_UART");
+        break;
+
+    case ESP_SLEEP_WAKEUP_WIFI:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_WIFI");
+        break;
+
+    case ESP_SLEEP_WAKEUP_COCPU:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_COCPU");
+        break;
+
+    case ESP_SLEEP_WAKEUP_COCPU_TRAP_TRIG:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_COCPU_TRAP_TRIG");
+        break;
+
+    case ESP_SLEEP_WAKEUP_BT:
+        ESP_LOGI(TAG, "ESP_SLEEP_WAKEUP_BT");
+        break;
+
+    default:
+        ESP_LOGI(TAG, "UNMATCH");
+        break;
+    }
+
+    vTaskDelay( pdMS_TO_TICKS(1000) );
+    // Go back to sleep, only the ULP Risc-V will run
+    ESP_LOGI(TAG, "ULP-RISC-V ulp running_cnt is %ld", ulp_running_cnt);
+    printf("Entering in deep sleep \n");
+    // Small delay to ensure the messages are printed
+    restart_ulp_program();
+    vTaskDelay( pdMS_TO_TICKS(1000) );
+
+    ESP_ERROR_CHECK( esp_sleep_enable_ulp_wakeup() );
     esp_deep_sleep_start();
 }
 
-static void init_ulp_program(void)
+static void start_ulp_program(void)
 {
     esp_err_t err = ulp_riscv_load_binary(ulp_main_bin_start, (ulp_main_bin_end - ulp_main_bin_start));
     ESP_ERROR_CHECK(err);
 
-    /* Start the program */
+    // Start the program
     err = ulp_riscv_run();
     ESP_ERROR_CHECK(err);
+}
+
+static void stop_ulp_program(void)
+{
+    ulp_riscv_timer_stop();
+    vTaskDelay( pdMS_TO_TICKS(1000) );
+}
+
+static void restart_ulp_program(void)
+{
+    ulp_riscv_timer_resume();
+    vTaskDelay( pdMS_TO_TICKS(1000) );
 }
--- ulp/main.c
+++ ulp/main.c
@@ -1,10 +1,9 @@
-/* ULP-RISC-V example
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-   This code runs on ULP-RISC-V  coprocessor
-*/
+// ULP-RISC-V example
+// This example code is in the Public Domain (or CC0 licensed, at your option.)
+// Unless required by applicable law or agreed to in writing, this
+// software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied.
+// This code runs on ULP-RISC-V  coprocessor
 
 #include <stdio.h>
 #include <stdint.h>
@@ -14,20 +13,52 @@
 #include "ulp_riscv_gpio.h"
 #include "soc/rtc_cntl_reg.h"
 
+#define GPIO_BUTTON GPIO_NUM_4
 #define GPIO_LED GPIO_NUM_5
+#define ULP_LOOP_MAX_COUNT 20
+
+uint32_t running_cnt = 0;
+uint32_t loop_cnt = 0;
+static bool gpio_level = false;
+bool gpio_level_previous = false;
 
 int main (void)
 {
+//    running_cnt += 1;
+    running_cnt++;
+//    loop_cnt = 0;
+    gpio_level = (bool)ulp_riscv_gpio_get_level(GPIO_BUTTON);
+    gpio_level_previous = gpio_level;
+
+    ulp_riscv_gpio_init(GPIO_BUTTON);
+    ulp_riscv_gpio_input_enable(GPIO_BUTTON);
     ulp_riscv_gpio_init(GPIO_LED);
     ulp_riscv_gpio_output_enable(GPIO_LED);
     ulp_riscv_gpio_set_output_mode(GPIO_LED, RTCIO_MODE_OUTPUT);
 
     while(1) {
-        ulp_riscv_gpio_output_level(GPIO_LED, 1);
-        ulp_riscv_delay_cycles(ULP_RISCV_CYCLES_PER_MS * 1000); // wait 1000 ms
-        ulp_riscv_gpio_output_level(GPIO_LED, 0);
-        ulp_riscv_delay_cycles(ULP_RISCV_CYCLES_PER_MS * 1000);
+        gpio_level = (bool)ulp_riscv_gpio_get_level(GPIO_BUTTON);
+        if (gpio_level != gpio_level_previous) {
+//            gpio_level_previous = gpio_level;
+            ulp_riscv_wakeup_main_processor();
+            break;
+        }
+        // ulp_riscv_delay_cycles(ULP_RISCV_CYCLES_PER_MS * 10);
+
+        if (loop_cnt < ULP_LOOP_MAX_COUNT) {
+            ulp_riscv_gpio_output_level(GPIO_LED, 1);
+            ulp_riscv_delay_cycles(ULP_RISCV_CYCLES_PER_MS * 1000); // wait 1000 ms
+            ulp_riscv_gpio_output_level(GPIO_LED, 0);
+            ulp_riscv_delay_cycles(ULP_RISCV_CYCLES_PER_MS * 1000);
+            loop_cnt++;
+        } else {
+            gpio_level_previous = (bool)ulp_riscv_gpio_get_level(GPIO_BUTTON);
+            loop_cnt = 0;
+            ulp_riscv_wakeup_main_processor();
+            break;
+        }
     }
-    
+
+    // ulp_riscv_halt() is called automatically when main exits
     return 0;
 }
